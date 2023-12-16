2f60259db139a5478ff03b50c12d2433589bedec
mattisnotbeef <154026248+mattisnotbeef@users.noreply.github.com>
2023-12-16 16:27:30 -0600

    Add files via upload

---
+++ fastandfuriousrandomizer.py
@@ -0,0 +1,6 @@
+#This script randomly chooses a movie from The Fast and the Furious franchise
+fast_movies = ["the_fast_and_the_furious" , "2_fast_2_furious" , "fast_and_furious_tokyo_drift" , "fast_&_furious" , "fast_five" , "fast_six" , "fast_seven" , "f8_of_the_furious" , "hobbs_and_shaw" , "fast_nine" , "fast_X"]
+
+import random
+
+print(random.choice(fast_movies))
